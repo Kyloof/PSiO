2325742c8e83046639fb488ff0deee8adcdfa4f0
Kyloof <jankubow.sr@gmail.com>
2024-12-07 11:35:40 +0100

    final

---
+++ projektHMS/angularclient/src/app/waiter-page/waiter-page.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HotelService } from '../service/hotel.service';
+import { OrderDto } from '../model/orderDto/orderDto';
+import { NgForm } from '@angular/forms';
+import { HttpClient, HttpErrorResponse, HttpStatusCode } from '@angular/common/http';
+import { Menu } from '../model/menu/menu';
+
+@Component({
+  selector: 'app-waiter-page',
+  templateUrl: './waiter-page.component.html',
+  styleUrls: ['./waiter-page.component.css']
+})
+export class WaiterPageComponent implements OnInit{
+  public errorMessage: string = "";
+  public orderDto: OrderDto = {
+    guestId: "",
+    dishIds: ""
+  }
+  public orders: Menu[] = [];
+
+  constructor(
+    private router: Router,
+    private hotelService: HotelService){
+    if(this.router.getCurrentNavigation()?.extras.state == undefined)
+      router.navigateByUrl('login');
+  }
+
+  public employeeId: any;
+  ngOnInit(): void {
+    this.employeeId = history.state;
+    console.log(this.employeeId.id);
+  }
+
+  public showPersonalData(): void {
+    this.router.navigateByUrl("/personal-data", {state: {id: this.employeeId.id}});
+  }
+
+  onSubmit(form: NgForm){
+    this.hotelService.acceptOrder(this.orderDto).subscribe(
+      (response: HttpStatusCode.Accepted) => {
+        confirm('Order accepted successfully');
+      },
+      (error) => {
+        this.errorMessage = 'Failed to accept order: ' + this.orderDto.dishIds
+        console.log(error);
+        confirm(this.errorMessage)
+      });
+  }
+
+
+
+}
